62d602cdee98422adf482b86feb7f26697b249e7
henry <henry.byrd@claricode.com>
2019-01-03 20:36:08 -0500

    Initial commit

---
+++ Karatsuba.Multiplier/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Karatsuba.Multiplier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
