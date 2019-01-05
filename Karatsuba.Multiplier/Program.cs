8809507eb95c66f89741c3932d3aa5bf23701053
henry <henry.byrd@claricode.com>
2019-01-05 17:47:54 -0500

    almost there, found a substring bug when numbers are different lengths

--- Karatsuba.Multiplier/Program.cs
+++ Karatsuba.Multiplier/Program.cs
@@ -6,28 +6,45 @@ namespace Karatsuba.Multiplier
     {
         static void Main(string[] args)
         {
-            var a = new StringBigInteger("12345678901234567890123456789012345678901234567890123456789012345678901234567890");
-            var b = new StringBigInteger("12345678901234567890123456789012345678901234567890123456789012345678901234567890");
-            PrintAddAndSubtract(a, b);
+            var c = new StringBigInteger("100");
+            var d = new StringBigInteger("25");
+            PrintAllOperations(c, d);
+
+            var a = new StringBigInteger("54");
+            var b = new StringBigInteger("12");
+            PrintAllOperations(a, b);
+
+            var a0 = new StringBigInteger("5454");
+            var b0 = new StringBigInteger("1212");
+            PrintAllOperations(a0, b0);
+
+            var a1 = new StringBigInteger("321");
+            var b1 = new StringBigInteger("345");
+            PrintAllOperations(a1, b1);
 
             var a2 = new StringBigInteger("54321");
             var b2 = new StringBigInteger("12345");
-            PrintAddAndSubtract(a2, b2);
+            PrintAllOperations(a2, b2);
 
-            var a3 = new StringBigInteger("1234");
-            var b3 = new StringBigInteger("321");
-            PrintAddAndSubtract(a3, b3);
+            // var a3 = new StringBigInteger("1234");
+            // var b3 = new StringBigInteger("321");
+            // PrintAllOperations(a3, b3);
 
-            var a4 = new StringBigInteger("1234");
-            var b4 = new StringBigInteger("123");
-            PrintAddAndSubtract(a4, b4);
+            // var a4 = new StringBigInteger("1234");
+            // var b4 = new StringBigInteger("123");
+            // PrintAllOperations(a4, b4);
+
+            // var a5 = new StringBigInteger("3141592653589793238462643383279502884197169399375105820974944592");
+            // var b5 = new StringBigInteger("2718281828459045235360287471352662497757247093699959574966967627");
+            // PrintAllOperations(a5, b5);
         }
 
-        static void PrintAddAndSubtract(StringBigInteger a, StringBigInteger b)
+        static void PrintAllOperations(StringBigInteger a, StringBigInteger b)
         {
-            PrintAdd(a, b);
-            PrintSubtract(a, b);
-            PrintSubtract(b, a);
+            // PrintAdd(a, b);
+            // PrintSubtract(a, b);
+            // PrintSubtract(b, a);
+            PrintMultiply(a, b);
         }
 
         static void PrintAdd(StringBigInteger a, StringBigInteger b)
@@ -38,6 +55,11 @@ namespace Karatsuba.Multiplier
         static void PrintSubtract(StringBigInteger a, StringBigInteger b)
         {
             Console.WriteLine($"{a} - {b} = {a - b}");
+        }
+
+        private static void PrintMultiply(StringBigInteger a, StringBigInteger b)
+        {
+            Console.WriteLine($"{a} * {b} = {a * b}");
         }
     }
 }
--- Karatsuba.Multiplier/StringBigInteger.cs
+++ Karatsuba.Multiplier/StringBigInteger.cs
@@ -8,6 +8,8 @@ namespace Karatsuba.Multiplier
         private string _value { get; set; }
         private bool _isNegative { get; set; }
 
+        private int Length => _value.Length;
+
         public StringBigInteger(string value)
         {
             if (string.IsNullOrWhiteSpace(value)) throw new ArgumentException("value must represent an integer");
@@ -43,7 +45,7 @@ namespace Karatsuba.Multiplier
         {
             int lastLeadingZero = -1;
             int i = 0;
-            while (i < value.Length && value[i] == '0') 
+            while (i < value.Length && value[i] == '0')
                 lastLeadingZero = i++;
             value = value.Substring(lastLeadingZero + 1);
             if (value == "") value = "0";
@@ -119,11 +121,75 @@ namespace Karatsuba.Multiplier
             return 0;
         }
 
+        public static StringBigInteger FlipSign(StringBigInteger value) => new StringBigInteger(value._value, !value._isNegative);
+
         public static StringBigInteger operator +(StringBigInteger a, StringBigInteger b) => Add(a, b);
 
         public static StringBigInteger operator -(StringBigInteger a, StringBigInteger b) => Add(a, FlipSign(b));
 
-        public static StringBigInteger FlipSign(StringBigInteger value) => new StringBigInteger(value._value, !value._isNegative);
+        public static StringBigInteger FullKWrapper(StringBigInteger a, StringBigInteger b)
+        {
+            var isNegative = a._isNegative ^ b._isNegative;
+            var posA = a._isNegative ? FlipSign(a) : a;
+            var posB = b._isNegative ? FlipSign(b) : b;
+            var result = KMultiply(a, b);
+            return isNegative ? FlipSign(result) : result;
+        }
+
+        public static StringBigInteger KMultiply(StringBigInteger x, StringBigInteger y)
+        {
+            if (x.Length == 1 && y.Length == 1) return SingleDigitMultiply(x, y);
+            var compare = CompareIgnoreSign(x, y);
+            var bigger = compare >= 0 ? x : y;
+            var smaller = compare >= 0 ? y : x;
+            var n = bigger.Length;
+            var n_2 = n / 2;
+            var a = Top(bigger, n_2);
+            var b = Bottom(bigger, n_2);
+            var c = Top(smaller, n_2);
+            var d = Bottom(smaller, n_2);
+
+            Console.WriteLine($"... computing {x} * {y}");
+            Console.WriteLine($"... length of bigger number: {n}");
+            Console.WriteLine($"... a: {a}, b: {b}, c: {c}, d: {d}");
+            var ac = KMultiply(a,c);
+            var bd = KMultiply(b,d);
+            var ad_bc = KMultiply(a + b, c + d) - ac - bd;
+            Console.WriteLine($"... ac: {ac}, bd: {bd}, ad+bc: {ad_bc}");
+
+            var ac_n = MultiplyByTens(ac, n);
+            var ad_bc_n_2 = MultiplyByTens(ad_bc, n_2);
+            var result = ac_n + ad_bc_n_2 + bd;
+            Console.WriteLine($"... final result: {ac_n} + {ad_bc_n_2} + {bd} = {result}");
+
+            return result;
+        }
+
+        private static StringBigInteger SingleDigitMultiply(StringBigInteger x, StringBigInteger y)
+            => new StringBigInteger(((x._value[0] - '0') * (y._value[0] - '0')).ToString());
+
+        private static StringBigInteger Top(StringBigInteger x, int n)
+        {
+            if (n == 0) return new StringBigInteger("1");
+            if (n >= x.Length) return x;
+            return new StringBigInteger(x._value.Substring(0, n));
+        }
+
+        private static StringBigInteger Bottom(StringBigInteger x, int n)
+        {
+            if (n == 0) return x;
+            if (n >= x.Length) return new StringBigInteger("1");
+            return new StringBigInteger(x._value.Substring(n));
+        }
+
+        private static StringBigInteger MultiplyByTens(StringBigInteger ac, int n)
+        {
+            if (ac._value == "0") return ac;
+            var value = ac._value.PadRight(n + ac.Length, '0');
+            return new StringBigInteger(value);
+        }
+
+        public static StringBigInteger operator *(StringBigInteger a, StringBigInteger b) => FullKWrapper(a, b);
 
         public override string ToString()
         {
